1a9c965fab4942889deac0de8d2d71218669c9a6
shikhaii <shikha@innovateinfinite.com>
2021-11-01 16:26:01 +0530

    akalpya content changes

--- Akalpya2/dark/mail.php
+++ Akalpya2/dark/mail.php
@@ -6,7 +6,7 @@ $company = $_POST["company"];
 $message = $_POST["message"];
 
 
-$EmailTo = "000.lunchbox@gmail.com";
+$EmailTo = "shikhachauhan2110@gmail.com";
 $Subject = "New Message Received";
 
 // prepare email body text
